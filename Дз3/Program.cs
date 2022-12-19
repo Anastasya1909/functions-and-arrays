c8c41a39c2226abd3772ffcb5e23b7c573bedb91
Anastasya <ocenka.da@gmail.com>
2022-12-19 23:23:08 +0700

    Является ли день выходным?

---
+++ Дз3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Является ли день выходным?
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("да");
+  }
+
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("введите цифру от 1 до 7");
+  }
+
+  else Console.WriteLine("нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
