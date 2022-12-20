75159a0c777d5aa64739b969ceed932c2152f0c5
Anastasya <ocenka.da@gmail.com>
2022-12-20 19:48:27 +0700

    Является ли число А квадратом число В

---
+++ Square root/Program.cs
@@ -0,0 +1,17 @@
+﻿// Является ли число А квадратом число В
+Console.WriteLine("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+
+double result = Math.Pow(numberB,2);
+
+if (numberA == result)
+{
+System.Console.WriteLine($"да");
+}
+else 
+{
+System.Console.WriteLine($"нет");  
+}
