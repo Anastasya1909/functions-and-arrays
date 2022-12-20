f54e27a27935cda3347aa3bd0484eb945a06f388
Anastasya <ocenka.da@gmail.com>
2022-12-20 19:10:56 +0700

    Квадрат числа

---
+++ MathPow/Program.cs
@@ -0,0 +1,6 @@
+﻿// Квадрат числа
+Console.WriteLine("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+double result = Math.Pow(numberA,2);
+System.Console.WriteLine($"квадрат от числа {numberA} равен {result}");
+
