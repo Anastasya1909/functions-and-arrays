2d10f993ba5721be9af29ebb647e89ae57407f53
Anastasya <ocenka.da@gmail.com>
2022-12-20 16:57:50 +0700

    Принять координаты и определить четверть

---
+++ x, y/Program.cs
@@ -0,0 +1,35 @@
+﻿// Принять координаты и определить четверть
+Console.Write("Введите х: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+
+int qwarter = 0;
+if (x > 0)
+{
+    if (y < 0)
+    {
+        qwarter = 4;
+    }
+    else
+    {
+        qwarter = 1;
+    }
+}
+else
+{
+    if (y > 0)
+    {
+        qwarter = 2;
+    }
+    else
+    {
+        qwarter = 3;
+    }
+};
+
+Console.WriteLine(qwarter + " " + "qwater");
+         
+
+ 
