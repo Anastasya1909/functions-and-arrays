5fd0d2d8d1c107d66c1a423472d9ae2aa240d7a4
Anastasya <ocenka.da@gmail.com>
2022-12-20 19:59:23 +0700

    Число и день недели

---
+++ day of week/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int i = int.Parse(Console.ReadLine());
+
+if (i == 1)
+{
+    System.Console.WriteLine("понедельник");
+}
+else if (i == 2)
+{
+    System.Console.WriteLine("вторник");
+}
+else if (i == 3)
+{
+    System.Console.WriteLine("среда");
+}
+else if (i == 4)
+{
+    System.Console.WriteLine("четверг");
+}
+else if (i == 5)
+{
+    System.Console.WriteLine("пятница");
+}
+else if (i == 6)
+{
+    System.Console.WriteLine("суббота");
+}
+else if (i == 7)
+{
+    System.Console.WriteLine("воскресенье");
+};
+
