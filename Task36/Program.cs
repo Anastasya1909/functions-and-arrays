3733def4fb6341e57ad4cbb604d224d98903d184
Anastasya <ocenka.da@gmail.com>
2022-12-23 18:04:16 +0700

    Найдите сумму элементов, стоящих на нечётных позициях

---
+++ Task36/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int length = ReadInt ("Array length: ");
+int[] array = GetRandomArray(length, -10, 10);
+int sum = FindElements(array);
+
+PrintArray(array);
+System.Console.WriteLine();
+int count = FindElements(array);
+System.Console.WriteLine(count);
+
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int [] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+
+void PrintArray(int [] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+int FindElements(int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];      
+    }
+    return sum;
+}
