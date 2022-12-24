32b51f6e10ce623681085e881347812327cef72b
Anastasya <ocenka.da@gmail.com>
2022-12-24 15:18:52 +0700

    M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите элементы через пробел: ");
+int [] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Transform(array);
+int count = Transform(array);
+
+
+int Transform(int [] array)
+{
+    int count = 0;
+    for (int i= 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+    Console.WriteLine($"Количество элементов положительных (>0): {count} ");
