82748db9333e675b92ebcabd6259da39b64e61ce
Anastasya <ocenka.da@gmail.com>
2022-12-24 13:54:08 +0700

    Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1

---
+++ Task44/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 44 Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+int N = ReadInt("N");
+Fibonacci(N);
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+void Fibonacci(int N)
+{
+    int[] array = new int[N];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < N; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    PrintArray(array);
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
