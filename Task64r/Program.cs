67c286635ea760ad203567688978c7b351f8d32a
Anastasya <ocenka.da@gmail.com>
2022-12-29 13:17:54 +0700

    натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии

---
+++ Task64r/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Task64();
+
+void Task64()
+{
+    int b = 1;
+    int N = ReadInt("N: ");
+    System.Console.WriteLine(NumbersRec(b, N));
+}
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Input {argument}");
+    return int.Parse(Console.ReadLine());
+}
+string NumbersRec(int N, int b)
+{
+    if (N <= b) return NumbersRec(N + 1, b) + $"{N} ";
+    else return String.Empty;       
+}
+
