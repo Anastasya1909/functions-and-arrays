bb5de37dd29d710cdf69e5a358114a03b5d498d7
Anastasya <ocenka.da@gmail.com>
2022-12-29 14:03:39 +0700

    найдёт сумму натуральных элементов в промежутке от M до N

---
+++ Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Task66();
+
+void Task66()
+{
+    
+    int M = ReadInt("M: ");
+    int N = ReadInt("N: ");
+    if(IsPossible(M, N))
+    {
+        System.Console.WriteLine("enter a natural number (number > 0)");
+        return;
+    }
+    System.Console.WriteLine(SumElements(M, N));
+}
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Input {argument}");
+    return int.Parse(Console.ReadLine());
+}
+
+int SumElements (int M, int N)
+{
+    int result = 0;
+    for (int i = M; i <= N; i++) result += i;
+    return result;
+}
+
+bool IsPossible(int M, int N)
+{
+    return M < 0 && N < 0 || N < 0 || M < 0 ;
+}
